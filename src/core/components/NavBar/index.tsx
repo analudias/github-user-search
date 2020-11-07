b64f6a0cbff165d4c178be13eb9b85494a2af320
Ana Luisa Dias <analudias@live.com>
2020-11-07 16:20:08 -0300

    Adiciona barra de navegação superior

---
+++ src/core/components/NavBar/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './styles.css';
+
+
+const NavBar = () => (
+    <nav className="main-nav">
+        <div className="main-title">
+            <h4>Bootcamp DevSuperior</h4>
+        </div>
+    </nav>
+);
+
+export default NavBar;
